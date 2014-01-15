707d2361fd9eb5c17e1903f1a024840676ad2c32
hotoo <hotoo.cn@gmail.com>
2014-01-15 16:26:21 +0800

    A: make build

--- dist/spassword-debug.js
+++ dist/spassword-debug.js
@@ -1,5 +1,5 @@
-define("arale/spassword/1.0.0/spassword-debug", [ "$-debug", "arale/events/1.1.0/events-debug" ], function(require, exports, module) {
-    //require("./spassword.css");
+define("alipay/spassword/1.0.0/spassword-debug", [ "./spassword-debug.css", "$-debug", "arale/events/1.1.0/events-debug" ], function(require, exports, module) {
+    require("./spassword-debug.css");
     var $ = require("$-debug");
     var Events = require("arale/events/1.1.0/events-debug");
     var DEFAULT_LENGTH = 6;
@@ -72,7 +72,7 @@ define("arale/spassword/1.0.0/spassword-debug", [ "$-debug", "arale/events/1.1.0
             return this;
         },
         focus: function() {
-            focusInput(this._element);
+            focusInput(this._element)();
             return this;
         },
         blur: function() {
@@ -95,4 +95,8 @@ define("arale/spassword/1.0.0/spassword-debug", [ "$-debug", "arale/events/1.1.0
         return new Array(times + 1).join(string);
     }
     module.exports = spassword;
+});
+
+define("alipay/spassword/1.0.0/spassword-debug.css", [], function() {
+    seajs.importStyle("input.spassword{position:absolute;color:#fff;opacity:0;width:1px;height:1px;font-size:1px;left:0;-webkit-box-sizing:content-box;box-sizing:content-box;-webkit-user-select:initial}div.spassword{display:-webkit-box;border:1px solid #666;-webkit-border-radius:4px;background:#ebebeb;width:100%;height:40px;-webkit-box-sizing:border-box;cursor:text;background:#fff}div.spassword.active{border-color:rgba(82,168,236,.8);outline:0;outline:thin dotted \\9;-webkit-box-shadow:inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(82,168,236,.6);-moz-box-shadow:inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(82,168,236,.6);box-shadow:inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(82,168,236,.6)}div.spassword i{display:block;-webkit-box-flex:1;background:0;padding:15px 0}div.spassword i:nth-child(n+2){border-left:1px solid #ccc}div.spassword b{display:block;margin:0 auto;width:10px;height:10px;overflow:hidden;background:#000;-webkit-border-radius:10px;visibility:hidden}");
 });
